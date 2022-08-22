102d3d4c41d469ad4eea6ab5d4eb632f79ce3a7c
nafiz <ndin@cisscom.com>
2022-08-22 18:25:06 +0600

    started once again

--- 1/main.js
+++ 1/main.js
@@ -1,6 +1,6 @@
 //! https://www.mathsisfun.com/index.htm >>>> math
 //! 1. Lynda.com - Learning 3D Graphics on the Web with Three.js (2017 ENG) >>>> course
-//! 01_05 >>>> episode
+//! 2.4 >>>> episode
 // console.log(THREE);
 
 //! Creating a Scene
@@ -24,20 +24,26 @@ const box = new THREE.BoxGeometry(1, 1, 1);
 const boxmat = new THREE.MeshBasicMaterial( {color: 0xFF00FF} );
 
 
+//! creating a caps with geometry
+
 const caps = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
 const capsmat = new THREE.MeshBasicMaterial( {color: 0xFF00FF} );
 
+
+//! creating a plane with geometry
 
 const plane = new THREE.PlaneGeometry(4, 4);
 const planemat = new THREE.MeshBasicMaterial( {color: 0x00FF00, side: THREE.DoubleSide} );
 
 
+//! creating a ring with geometry
+
 const ring = new THREE.RingGeometry( 6, 4, 30, 20, 4, 6.283185307179586 );
 const ringmat = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
 
 
-// const mesh = new THREE.Mesh( box, boxmat );
-const mesh = new THREE.Mesh( caps, capsmat );
+const mesh = new THREE.Mesh( box, boxmat );
+// const mesh = new THREE.Mesh( caps, capsmat );
 const ringMesh = new THREE.Mesh( ring, ringmat );
 
 // mesh.rotation.x = 10
@@ -57,24 +63,29 @@ const canvas = document.getElementById('web-gl-canvas')
 
 
 //* configs
-scene.add( ringMesh );
 ringMesh.add( mesh );
+scene.add( ringMesh );
+
+// naming an object;
+ringMesh.name = 'the-ring';
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 canvas.appendChild(renderer.domElement)
 
-renderer.render( scene, camera )
 
-// function animate() {
-//     requestAnimationFrame( animate );
+// const theObj = scene.getObjectByName('the-ring')
+// console.log({theObj});
+mesh.rotation.z -= 2
 
-//     mesh.rotation.z += 0.0001
-//     mesh.rotation.x += 0.005
+function animate() {
+    requestAnimationFrame( () => animate() );
 
-//     ringMesh.rotation.x += 0.14
-//     ringMesh.rotation.y += 0.001
+    mesh.rotation.x += 0.055
 
-//     renderer.render( scene, camera );
-// };
-// animate();
+    ringMesh.rotation.x += 0.05
+    ringMesh.rotation.y += 0.001
+
+    renderer.render( scene, camera );
+};
+animate();
